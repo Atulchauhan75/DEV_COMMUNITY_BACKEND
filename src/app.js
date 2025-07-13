065d644026bb9c9ac87e9dcd4a6e2806ec709ae1
Atulchauhan75 <achauhan2161@gmail.com>
2025-07-13 19:02:07 +0530

    Created Expess Server.

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.use("/", (req, res) => {
+  res.send("Root of the, World!");
+});
+app.use("/test", (req, res) => {
+  res.send("TESTING, World!");
+});
+app.use("/hello", (req, res) => {
+  res.send("Hello, World!");
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
